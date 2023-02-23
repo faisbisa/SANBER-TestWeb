74866c3310c89af176338cff4ce1158f5a1db5d0
faisbisa <faisalkurnis12@gmail.com>
2023-02-23 16:48:06 +0700

    user login kasir aja

--- cypress/support/e2e/login.js
+++ cypress/support/e2e/login.js
@@ -3,4 +3,19 @@
         cy.visit('https://kasirdemo.belajarqa.com')
         cy.url().should('include', '/login')
     })
+//open kasir aja login page
+    it('user success login to kasir Aja', ()=> {
+        cy.visit('https://kasirdemo.belajarqa.com')
+        // cy.url().should('include', '/login')
+
+//Input username pass   
+        cy.get('#email').type("sanber123@mail.com")
+        cy.get('#password').type("sanber123@")
+//click login
+        cy.get('#root > div > div > div > div.css-1w7v3tn > div > button').click()
+//assert dashboard after login
+        // cy.url().should('include','/dashboard')
+        cy.get('#root > div > div > div.css-tnxwfz > div > h3').click()
+        cy.contains("kasirAja")
+    })
  })
